a7373aeb95b473db3a058d02307a130b2591cc41
abonArtem <students.dev.project@gmail.com>
2017-01-16 00:54:47 +0200

    Queue subscription example was added

--- MessageBroker/RabbitMqService/AmqpMessagingService.cs
+++ MessageBroker/RabbitMqService/AmqpMessagingService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
 
 namespace RabbitMqService
 {
@@ -16,6 +17,9 @@ namespace RabbitMqService
         private string _exchangeName = "";
         private string _oneWayMessageQueueName = "OneWayMessageQueue";
         private string _workerQueueDemoQueueName = "WorkerQueueDemoQueue";
+        private string _publishSubscribeExchangeName = "PublishSubscribeExchange";
+        private string _publishSubscribeQueueOne = "PublishSubscribeQueueOne";
+        private string _publishSubscribeQueueTwo = "PublishSubscribeQueueTwo";
         private bool _durable = true;
 
         public IConnection GetRabbitMqConnection()
@@ -87,6 +91,49 @@ namespace RabbitMqService
             var messageBytes = Encoding.UTF8.GetBytes(message);
 
             model.BasicPublish(_exchangeName, _workerQueueDemoQueueName, basicProperties, messageBytes);
+        }
+
+        public void SetUpExchangeAndQueuesForDemo(IModel model)
+        {
+            model.ExchangeDeclare(_publishSubscribeExchangeName, ExchangeType.Fanout, true);
+            model.QueueDeclare(_publishSubscribeQueueOne, true, false, false, null);
+            model.QueueDeclare(_publishSubscribeQueueTwo, true, false, false, null);
+            model.QueueBind(_publishSubscribeQueueOne, _publishSubscribeExchangeName, "");
+            model.QueueBind(_publishSubscribeQueueTwo, _publishSubscribeExchangeName, "");
+        }
+
+        public void SendMessageToPublishSubscribeQueue(string message, IModel model)
+        {
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.Persistent = _durable;
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            model.BasicPublish(_publishSubscribeExchangeName, "", basicProperties, messageBytes);
+        }
+
+        public void ReceivePublishSubscribeMessageReceiverOne(IModel model)
+        {
+            model.BasicQos(0, 1, false);
+            Subscription subscription = new Subscription(model, _publishSubscribeQueueOne, false);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = subscription.Next();
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message from queue: {0}", message);
+                subscription.Ack(deliveryArguments);
+            }
+        }
+
+        public void ReceivePublishSubscribeMessageReceiverTwo(IModel model)
+        {
+            model.BasicQos(0, 1, false);
+            Subscription subscription = new Subscription(model, _publishSubscribeQueueTwo, false);
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = subscription.Next();
+                String message = Encoding.UTF8.GetString(deliveryArguments.Body);
+                Console.WriteLine("Message from queue: {0}", message);
+                subscription.Ack(deliveryArguments);
+            }
         }
     }
 
